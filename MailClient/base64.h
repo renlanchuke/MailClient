75384bd1651949e92f16309618590be2d6919a12
renlanchuke <1065730562@qq.com>
2015-11-27 21:35:08 +0800

    Add base64

---
+++ MailClient/base64.h
@@ -0,0 +1,8 @@
+#ifndef _BASE64_H_
+#define _BASE64_H_
+#include"container.h"
+//编码第一个参数输出String类型，第二参数输入一个C类型的字符串
+void base64Encode(String* ,unsigned char const* , unsigned int len);
+//解码第一个参数输出String,第二个参数已编码好字符串
+void base64Decode(String* , String const *);
+#endif
